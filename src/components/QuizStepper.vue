5f0e5dedc8bb322f3da063c326afaf17da359cb2
hassan <hassan.azarbaijani@gmail.com>
2021-08-24 21:00:45 +0430

    score added && new component created for quiz stepper

---
+++ src/components/QuizStepper.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step
+        v-for="(q, i) in question"
+        :key="i"
+        :complete="e1 > i + 1"
+        step="i"
+      >
+        Question {{ i + 1 }}
+      </v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items v-for="(x, index) in question" :key="index">
+      <v-stepper-content :step="index + 1">
+        <h3 class="pa-5 headline">
+          {{ x.question }}
+        </h3>
+        <v-card
+          class="mb-10 pa-4 display-1 rounded-lg"
+          color=" lighten-1"
+          height="200px"
+        >
+          <v-row justify="space-around">
+            <v-radio-group
+              class="pa-1"
+              v-for="answer in x.answers"
+              :key="answer"
+              @change="selected($event, index)"
+              v-model="radioGroup"
+            >
+              <v-radio :key="index" :label="answer" :value="answer"></v-radio>
+            </v-radio-group>
+          </v-row>
+        </v-card>
+
+        <v-btn color="primary" @click="nextStep()"> Next </v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+    {{scores}}
+  </v-stepper>
+</template>
+
+<script>
+export default {
+  props: {
+    question: Array,
+  },
+  data: () => ({
+    e1: 1,
+    radioGroup: 1,
+    score: 0,
+    scores: []
+  }),
+  methods: {
+    selected(event, index) {
+      if (event === this.question[index].correctAnswer) {
+          this.score++
+          this.addScore(this.score)
+      }else{
+          console.log(event ,'wrong')
+            if(this.question[index]){
+                this.score = 0
+                this.scores.splice(index,1)
+            }
+      }
+    },
+    addScore(score){
+        this.scores.push(score)
+    },
+    nextStep(){
+        this.e1 += 1 
+        this.radioGroup = 0
+    }
+  },
+};
+</script>
+
+<style>
+</style>
